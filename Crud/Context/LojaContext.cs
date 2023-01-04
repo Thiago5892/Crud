9271ec014874558624fdff3840144a13d0b91236
Thiago <Thiago@DESKTOP-CONMSLV>
2023-01-04 20:19:35 -0300

    Adicionar arquivos de projeto.

---
+++ Crud/Context/LojaContext.cs
@@ -0,0 +1,15 @@
+﻿using Crud.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Crud.Context
+{
+    public class LojaContext : DbContext
+    {
+        public DbSet<Produto> Produto { get; set; }
+
+    }
+}
